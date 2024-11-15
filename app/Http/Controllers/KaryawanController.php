2aebe60da997ac85ba51230dff92102ccbefca11
Akbar Hossam Delmiro <akbarhossam123@gmail.com>
2024-11-16 03:50:27 +0700

    search dan pagination

--- app/Http/Controllers/KaryawanController.php
+++ app/Http/Controllers/KaryawanController.php
@@ -2,16 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Karyawan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class KaryawanController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $karyawan = DB::table('karyawan')->orderBy('nama_lengkap')
-            ->join('departemen', 'karyawan.kode_dept', '=', 'departemen.kode_dept')
-            ->get();
-        return view('karyawan.index', compact('karyawan'));
+        // $karyawan = DB::table('karyawan')->orderBy('nama_lengkap')
+        //     ->join('departemen', 'karyawan.kode_dept', '=', 'departemen.kode_dept')
+        //     ->paginate(1);
+        $query = Karyawan::query();
+        $query->select('karyawan.*', 'nama_dept');
+        $query->join('departemen', 'karyawan.kode_dept', '=', 'departemen.kode_dept');
+        $query->orderBy('nama_lengkap');
+        if (!empty($request->nama_karyawan)) {
+            $query->where('karyawan.nama_lengkap', 'like', '%' . $request->nama_karyawan . '%');
+        }
+        if (!empty($request->kode_dept)) {
+            $query->where('karyawan.kode_dept', 'like', '%' . $request->kode_dept . '%');
+        }
+        $karyawan = $query->paginate(1);
+        $department = DB::table('departemen')->get();
+        return view('karyawan.index', compact('karyawan', 'department'));
     }
 }
--- resources/views/karyawan/index.blade.php
+++ resources/views/karyawan/index.blade.php
@@ -23,47 +23,97 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-body">
-                            <table class="table table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th>No</th>
-                                        <th>NIK</th>
-                                        <th>Nama</th>
-                                        <th>Jabatan</th>
-                                        <th>No HP</th>
-                                        <th>Foto</th>
-                                        <th>Departemen</th>
-                                        <th>Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @forelse ($karyawan as $item)
-                                        <tr>
-                                            <td>{{ $loop->iteration }}</td>
-                                            <td>{{ $item->nik }}</td>
-                                            <td>{{ $item->nama_lengkap }}</td>
-                                            <td>{{ $item->jabatan }}</td>
-                                            <td>{{ $item->no_hp }}</td>
-                                            <td>
-                                                @if (empty($item->foto))
-                                                    <img class="avatar" src="{{ asset('no-foto.jpg') }}"
-                                                        alt="{{ $item->nama_lengkap }}">
-                                                @else
-                                                    <img class="avatar"
-                                                        src="{{ Storage::url('uploads/karyawan/' . $item->foto) }}"
-                                                        alt="{{ $item->nama_lengkap }}">
-                                                @endif
-                                            </td>
-                                            <td>{{ $item->nama_dept }}</td>
-                                            <td>
-                                                <a href="" class="btn btn-sm btn-primary">Edit</a>
-                                                <a href="" class="btn btn-sm btn-danger">Delete</a>
-                                            </td>
-                                        </tr>
-                                    @empty
-                                    @endforelse
-                                </tbody>
-                            </table>
+                            <div class="row">
+                                <div class="col-12">
+                                    <form action="{{ route('karyawan.index') }}" method="GET">
+                                        <div class="row">
+                                            <div class="col-6">
+                                                <div class="form-group">
+                                                    <input type="text" name="nama_karyawan" id="nama_karyawan"
+                                                        class="form-control" placeholder="Nama karyawan"
+                                                        value="{{ Request('nama_karyawan') }}">
+                                                </div>
+                                            </div>
+                                            <div class="col-4">
+                                                <div class="form-group">
+                                                    <select name="kode_dept" id="kode_dept" class="form-select">
+                                                        <option value="" hidden>Departemen</option>
+                                                        @forelse ($department as $d)
+                                                            <option
+                                                                {{ Request('kode_dept') == $d->kode_dept ? 'selected' : '' }}
+                                                                value="{{ $d->kode_dept }}">{{ $d->nama_dept }}</option>
+                                                        @empty
+                                                            <option value="">Tidak ada data</option>
+                                                        @endforelse
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="col-2">
+                                                <div class="form-group">
+                                                    <button type="submit" class="btn btn-primary">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                                                            fill="none" stroke="currentColor" stroke-linecap="round"
+                                                            stroke-linejoin="round" width="24" height="20"
+                                                            stroke-width="2">
+                                                            <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
+                                                            <path d="M21 21l-6 -6"></path>
+                                                        </svg>
+                                                        Cari
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-12">
+                                    <table class="table table-bordered">
+                                        <thead>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>NIK</th>
+                                                <th>Nama</th>
+                                                <th>Jabatan</th>
+                                                <th>No HP</th>
+                                                <th>Foto</th>
+                                                <th>Departemen</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @forelse ($karyawan as $item)
+                                                <tr>
+                                                    <td>{{ $loop->iteration + $karyawan->firstItem() - 1 }}</td>
+                                                    <td>{{ $item->nik }}</td>
+                                                    <td>{{ $item->nama_lengkap }}</td>
+                                                    <td>{{ $item->jabatan }}</td>
+                                                    <td>{{ $item->no_hp }}</td>
+                                                    <td>
+                                                        @if (empty($item->foto))
+                                                            <img class="avatar" src="{{ asset('no-foto.jpg') }}"
+                                                                alt="{{ $item->nama_lengkap }}">
+                                                        @else
+                                                            <img class="avatar"
+                                                                src="{{ Storage::url('uploads/karyawan/' . $item->foto) }}"
+                                                                alt="{{ $item->nama_lengkap }}">
+                                                        @endif
+                                                    </td>
+                                                    <td>{{ $item->nama_dept }}</td>
+                                                    <td>
+                                                        <a href="" class="btn btn-sm btn-primary">Edit</a>
+                                                        <a href="" class="btn btn-sm btn-danger">Delete</a>
+                                                    </td>
+                                                </tr>
+                                            @empty
+                                            @endforelse
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+
+                            {{ $karyawan->links('vendor.pagination.bootstrap-5') }}
+
                         </div>
                     </div>
 
